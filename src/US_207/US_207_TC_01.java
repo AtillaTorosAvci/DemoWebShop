f4eac25a373b89da632ae6b8f25b894469afc0e5
Atilla Avci <atillatorosavci@hotmail.com>
2025-03-28 22:44:29 +0100

    Proje bitti!

--- src/US_207/US_207_TC_01.java
+++ src/US_207/US_207_TC_01.java
@@ -6,29 +6,45 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-public class US_207_TC_1 extends BaseDriver {
+public class US_207_TC_01 extends BaseDriver {
 
     @Test
     public void Test () {
         driver.get("https://demowebshop.tricentis.com/");
+        MyFunc.Bekle(1);
+
+        WebElement voteExlnt1=driver.findElement(By.id("pollanswers-1"));
+        voteExlnt1.click();
+        MyFunc.Bekle(1);
+
+        WebElement vote1= driver.findElement(By.id("vote-poll-1"));
+        vote1.click();
+        MyFunc.Bekle(1);
 
         WebElement loginButton = driver.findElement(By.xpath("//a[@class='ico-login']"));
         loginButton.click();
+        MyFunc.Bekle(1);
 
         WebElement email = driver.findElement(By.xpath("//input[@class='email']"));
         email.sendKeys("atillapractice@hotmail.com");
+        MyFunc.Bekle(1);
 
         WebElement password = driver.findElement(By.xpath("//input[@class='password']"));
         password.sendKeys("GreenCard123");
+        MyFunc.Bekle(1);
 
         WebElement log_in = driver.findElement(By.xpath("//input[@value='Log in']"));
         log_in.click();
+        MyFunc.Bekle(1);
 
-        WebElement voteExlnt = driver.findElement(By.xpath("//input[@id='pollanswers-1']"));
-        voteExlnt.click();
+        WebElement voteExlnt2 = driver.findElement(By.xpath("//input[@id='pollanswers-1']"));
+        voteExlnt2.click();
+        MyFunc.Bekle(1);
+        //  oylama 1 kereliktir, giris yaptiktan sonra hata almaniz olasi
 
-        WebElement vote = driver.findElement(By.xpath("//input[@value='Vote']"));
-        vote.click();
+        WebElement vote2 = driver.findElement(By.xpath("//input[@value='Vote']"));
+        vote2.click();
+        MyFunc.Bekle(1);
 
         WebElement voteResult = driver.findElement(By.xpath("//ul[@class='poll-results']"));
         System.out.println("Vote Result = " + voteResult.getText());
